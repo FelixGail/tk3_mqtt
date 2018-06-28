799fb68a7619618cc12a9333956c057d4f9f11b4
Felix Gail <felix.clemens.gail@gmail.com>
2018-06-29 01:53:46 +0200

    set start id to 10

--- src/main/java/com/github/felixgail/tk3/mqtt/Player.java
+++ src/main/java/com/github/felixgail/tk3/mqtt/Player.java
@@ -10,7 +10,7 @@ public class Player {
   private String ip;
 
   private final static int modulo = 6;
-  private static int nextID = 27;
+  private static int nextID = 1;
   private static String getNextID() {
     String id = String.format("%d.%d.%d.%d.%d",
         genIDPart(4), genIDPart(3), genIDPart(2), genIDPart(1), genIDPart(0));
